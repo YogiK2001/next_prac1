91a9fd2cbe67f72661133d2f42e2d5cbab0ba55e
YogiK2001 <kaustubh7718@gmail.com>
2025-04-11 23:13:25 +0530

    added nav

---
+++ src/app/components/Nav.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <>
+         <div className="bg-gray-800" >
+
+         </div>
+        </>
+    );
+};
+
+export default Nav;
