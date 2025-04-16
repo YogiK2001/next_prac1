61809a11e8371d5e82e78e8a840e0d6257ca3226
YogiK2001 <kaustubh7718@gmail.com>
2025-04-16 23:08:18 +0530

    new logo

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import Link from "next/link";
 import Image from "next/image";
+import { signIn, signOut } from "next-auth/react"
+import { auth } from "@/l"
 
-const Nav = () => {
+const Nav = async () => {
+    const session = await auth();
+
+
     return (
         <header className="px-5 py-3 bg-white font-work-sans" >
             <nav className="flex flex-row justify-between items-center" >
                 <Link href="/">
                     <Image src="/logo.png" alt="logo" width={144} height={30} />
                 </Link>
-                <div className="text-2xl" > This is the Nav Bar</div>
+                <div className="felx items-center gap-5 text-black" >
+                    {session && session?.user ? (
+                        <>
+                            <Link href="/startup/create">
+                                <span>Create</span>
+                            </Link>
+
+                            <button onClick={() => signOut()} >
+                                SignOut
+                            </button>
+                            <Link href={`/user/${session?.id}`}>
+                                <span>{session?.user.name}</span>
+                            </Link>
+                        </>
+                    ): (
+                        <button onClick={() => signIn('github')} >
+                            <span> SignIn </span>
+                        </button>
+                    )}
+                </div>
             </nav>
         </header>
     );
